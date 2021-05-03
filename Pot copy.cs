cfde065206243bb6ef71b81c280ed847776162b1
some-leaves <41582660+some-leaves@users.noreply.github.com>
2021-05-03 14:54:32 -0500

    Add files via upload

---
+++ Pot copy.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Recursion
+{
+    class Pot
+    {
+       
+        
+     private int Diameter = 9;
+     private int miniumDiameter = 1;
+     private string measurmentSystem = "inches";
+     private int step = 1;
+     private int reduced = 2/3;
+
+
+
+      
+
+       
+
+      
+
+         
+        public Pot()
+        {
+            Console.WriteLine("Steps to draw a Dogon-like pot:");
+            DrawPot();
+            //ShrinkPot();
+
+        }
+
+        public  void ReduceIt()
+        {
+
+            Diameter = Diameter / 3;
+
+
+        }
+
+        private void DrawPot()
+        {
+            if (Diameter <= miniumDiameter)
+            {
+               
+                Console.WriteLine($"{step}. Draw the lid");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"{step}. Draw a circle that is {Diameter} {measurmentSystem} in size");
+            }
+              
+
+              ReduceIt();
+              step++;
+              DrawPot();
+
+        }
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program copy.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * // TODO: Kaveonna Gamble
+ * Recursion Assignment
+ *  //TODO: 02/23/2021
+ *   Based on Dogon Pot pseudo code from the book
+ * African Fractals: Modern Computing and Indigenous Design by Ron Eglash
+ */
+using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dogon pot example.");
+
+            new Pot();
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
